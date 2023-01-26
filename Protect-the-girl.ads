fd052833f887ea301119f1952efb716e353cff83
chaimalk <108950943+chaimalk@users.noreply.github.com>
2023-01-26 22:27:59 +0100

    Create Protect-the-girl.ads

---
+++ Protect-the-girl.ads
@@ -0,0 +1,14 @@
+{
+  "AdsController": {
+    "adNetwork": "applovin",
+    "admobBanner": "ca-app-pub-3940256099942544/6300978111",
+    "admobInterstitial": "ca-app-pub-3940256099942544/1033173712",
+    "applovinBanner": "0a99647e220f86c4",
+    "applovinInterstitial": "1a03c7f58d6e0345",
+    "unityGameId": "5137499",
+    "testMode": true,
+    "unityBanner": "Banner_Android",
+    "unityInterstitial": "Interstitial_Android",
+    "reviewActive": true
+  }
+}
