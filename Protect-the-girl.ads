d9da9eecc0e10105a2edc7277344698ce02fb573
chaimalk <108950943+chaimalk@users.noreply.github.com>
2023-02-03 11:45:11 +0100

    Update Protect-the-girl.ads

--- Protect-the-girl.ads
+++ Protect-the-girl.ads
@@ -1,11 +1,11 @@
 {
   "AdsController": {
-    "adNetwork": "applovin",
+    "adNetwork": "unity",
     "admobBanner": "ca-app-pub-3940256099942544/6300978111",
     "admobInterstitial": "ca-app-pub-3940256099942544/1033173712",
-    "applovinBanner": "0a99647e220f86c4",
-    "applovinInterstitial": "1a03c7f58d6e0345",
-    "unityGameId": "5137499",
+    "applovinBanner": "2ba2a3aeec0e8d92",
+    "applovinInterstitial": "e8307029aae1ec01",
+    "unityGameId": "5147696",
     "testMode": true,
     "unityBanner": "Banner_Android",
     "unityInterstitial": "Interstitial_Android",
